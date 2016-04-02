a08f34e8551264fe97b1f98258f0dcaa0985e804
Richard Yee <rdyyc99@gmail.com>
2016-04-02 02:11:13 -0700

    imageview implementation for graph

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/LineGraphActivity.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/LineGraphActivity.java
@@ -4,12 +4,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.sam_chordas.android.stockhawk.R;
-import com.sam_chordas.android.stockhawk.service.PlotIntentService;
 
 /**
  * Created by richard on 3/30/16.
@@ -17,9 +17,11 @@ import com.sam_chordas.android.stockhawk.service.PlotIntentService;
 public class LineGraphActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = LineGraphActivity.class.getSimpleName();
-    private Intent mServiceIntent;
-
     public static final String EXTRA_SYMBOL_LINEGRAPHACTIVITY = "extra_symbol_linegraph";
+    private static final String BASE_URL = "http://chart.finance.yahoo.com/z?s=";
+    private static final String APPEND_URL = "&q=l&l=on&z=s";
+
+    private ImageView mLineGraphImage;
     public static Intent newIntent(Context context, String symbol){
         return new Intent(context, LineGraphActivity.class)
                 .putExtra(EXTRA_SYMBOL_LINEGRAPHACTIVITY, symbol);
@@ -28,10 +30,13 @@ public class LineGraphActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_line_graph);
+        mLineGraphImage = (ImageView) findViewById(R.id.line_graph_image);
         String symbol = getIntent().getStringExtra(EXTRA_SYMBOL_LINEGRAPHACTIVITY);
-        Log.e(LOG_TAG, symbol);
-        mServiceIntent = PlotIntentService.newIntent(this, symbol);
-        startService(mServiceIntent);
+        String days = "&t=7d";
+        Glide.with(this)
+                .load(BASE_URL + symbol + days + APPEND_URL)
+                .fitCenter()
+                .into(mLineGraphImage);
     }
 
     @Override
